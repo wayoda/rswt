0ca60e6d72ba2d5e8662f9cf7d626449ea9b2a71
wayoda <e.fahle@wayoda.org>
2015-05-06 12:04:11 +0200

    prepare upload to the CheeseShop

---
+++ README.rst
@@ -0,0 +1,33 @@
+rswt.py
+=======
+The `RobertSonics WavTrigger`_ is an audio player for wave files. Audio playback can be controlled through a serial port. 
+
+``rswt.py`` is a python library for the WavTrigger that uses the `python serial library`_ to control the audio player.
+
+Documentation
+-------------
+Documentation for the library is **Not Yet here**
+
+A users guide for the WavTrigger is on the `RobertSonics website`_
+
+Install
+-------
+The module supports Python 2.7 and Python 3.3 onwards::
+
+        pip install rswt
+
+or::        
+
+        pip3 install rswt
+
+Source code
+-----------
+
+The project is hosted on `Github`_
+
+
+.. _`RobertSonics WavTrigger` : http://robertsonics.com/wav-trigger/
+.. _`python serial library` : http://pyserial.sourceforge.net 
+.. _`RobertSonics website` : http://robertsonics.com/wav-trigger-online-user-guide
+.. _`Github` : https://github.com/wayoda/rswt
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+# this file was stolen/copied from https://github.com/hynek/pem
+
+import codecs
+import os
+import re
+
+from setuptools import setup
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding
+    """
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
+def find_version(*file_paths):
+    """
+    Build a path from *file_paths* and search for a ``__version__``
+    string inside.
+    """
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='rswt',
+    version=find_version("rswt.py"),
+    description='Control RobertSonics WavTrigger through serial port.',
+    long_description=(read('README.rst') + '\n\n' ),
+    url='https://github.com/wayoda/rswt/',
+    license="MIT",
+    author="Eberhard Fahle",
+    author_email='e.fahle@wayoda.org',
+    py_modules=['rswt'],
+    install_requires=['pyserial'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
